8f4607b78a888e3713058d753e52d1d2cff4c089
liliancharlotte <lilian.krieg@live.de>
2022-01-06 17:37:00 +0100

    delete table merch

--- api/models/Merch.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    attributes: {
-    name: { type: 'string', required: true },
-    preis: { type: 'string', required: true },
-    beschreibung: { type: 'string'},
-    bildpfad: { type: 'string' },
-    },
-   };
