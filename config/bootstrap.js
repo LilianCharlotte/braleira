035e16876d2858dbc240c43db169fa57a28943c5
liliancharlotte <lilian.krieg@live.de>
2022-01-06 13:22:16 +0100

    add admin lisa lindgren

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -60,7 +60,8 @@ module.exports.bootstrap = async function() {
 
   // By convention, this is a good place to set up fake data during development.
   await User.createEach([
-    { emailAddress: 'admin@example.com', fullName: 'Ryan Dahl', isSuperAdmin: true, password: await sails.helpers.passwords.hashPassword('abc123') },
+    { emailAddress: 'admin@example.com', fullName: 'Ryan Dahl', isSuperAdmin: true, password: await sails.helpers.passwords.hashPassword('muckefuck') },
+    { emailAddress: 'lisa@lindgren.com', fullName: 'Lisa Lindgren', isSuperAdmin: true, password: await sails.helpers.passwords.hashPassword('muckefuck')}
   ]);
 
   await Passformwohlfuehl.createEach([
